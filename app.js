fd17fed5ce0195e6079c879a4da1d46f1cf9876b
Carlos Menezes <carlosedusm@gmail.com>
2025-03-17 05:40:55 -0300

    Adicionado função adicionar amigo

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function todosAmigos() {
+    let ul = document.getElementById('listaAmigos');
+    ul.innerHTML = '';
+    for (n = 0; n < amigos.length; n++) {
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(amigos[n]));
+        ul.appendChild(li);
+    }
+}
+
+function adicionarAmigo() {
+    let amigo = document.querySelector("input").value;
+
+    if (amigo.length > 0) {
+        amigos.push(amigo);
+        todosAmigos();
+        limparInput();
+    } else {
+        alert('Por favor, insira um nome.');
+    }
+}
+
+function limparInput() {
+    amigo = document.querySelector("input");
+    amigo.value = '';
+}
